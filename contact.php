622f1068a058f4f7719def3f60c93a3262fa3af6
mimiwhite186 <52567953+mimiwhite186@users.noreply.github.com>
2019-07-16 02:59:44 -0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <title>Contact Sara</title>
+  <meta charset="utf-8" />
+  <meta name="robots" content="noindex,nofollow" />
+  <meta name="viewport" content="width=device-width" />
+  <link rel="stylesheet" href="css/portal.css" />
+  <link rel="stylesheet" href="css/nav.css" />
+  <link rel="stylesheet" href="css/forms.css" />
+ </head>
+ <body>
+     <header>
+     <h1>Sara Newman: WEB110 Portal Website</h1>
+     <nav class="topnav" id="myTopnav">
+       <a href="index.html">Welcome</a>
+       <a href="elements.html">HTML Elements</a>
+       <a href="big/index.html">BIG</a>
+       <a href="http://w3schools.com" target="_blank">W3 Schools</a>
+       <a href="https://www.linkedin.com/learning/" target="_blank">LinkedIn Learning</a>
+       <a href="fp/index.html">Final Project</a>
+       <a href="contact.php" class="active">Contact Sara</a>
+       <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+    </nav>
+     </header>
+     
+   <main class="wrapper">
+       <h2 class="subheader">Contact Sara</h2>
+       		<!-- START HTML FORM -->
+	<form action="contact.php" method="post">
+	<div>
+		<label>
+			Name:<br /><input type="text" name="Name" required="required" placeholder="Full Name (required)" title="Name is required" tabindex="10" size="44" autofocus />
+		</label>
+	</div>
+	<div>	
+		<label>
+			Email:<br /><input type="email" name="Email" required="required" placeholder="Email (required)" title="A valid email is required" tabindex="20" size="44" />
+		</label>
+	</div>
+	<!-- below change the HTML to your form elements - only 'Name' & 'Email' (above) are significant -->
+	<div>	
+		<label>
+			Comments:<br /><textarea name="Comments" cols="36" rows="4" placeholder="Your comments are important to us!" tabindex="30"></textarea>
+		</label>
+	</div>	
+	<div></div>
+    <div class="g-recaptcha" data-sitekey="6LeDaSoUAAAAACnEiqA3QAkiRU-Q_wtk0vuBa_OX"></div> 
+	<div>
+		<input type="submit" value="submit" />
+	</div>
+    </form>
+	<!-- END HTML FORM -->
+    <script type="text/javascript"
+        src="https://www.google.com/recaptcha/api.js?hl=en">
+    </script>
+     <footer>
+      <p><small>&copy; 2019 by <a href="contact.php">Contact Sara Newman</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+    </footer>
+  </main>
+     
+  <!-- Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon -->
+     
+  <script>
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }   
+  </script>
+     
+ </body>
+</html>
